b816a0dcccb219656d34f1967bf1c81ed177f559
Thainá Zerbinatti <129007405+thaizerbinatti@users.noreply.github.com>
2024-02-29 17:50:48 -0300

    Update automacaoMonday.py

--- automacaoMonday.py
+++ automacaoMonday.py
@@ -9,7 +9,7 @@ navegadorFake = webdriver.Chrome()
 navegadorFake.get('https://totvs-torres.monday.com')
 loadTime.sleep(2)
 
-loginInput = navegadorFake.find_element(By.ID,'user_email').send_keys('thatazerb@gmail.com')
+loginInput = navegadorFake.find_element(By.ID,'user_email').send_keys('teste')
 passwordInput = navegadorFake.find_element(By.ID, 'user_password').send_keys('teste')
 loadTime.sleep(2)
 
@@ -35,10 +35,3 @@ loadTime.sleep(5)
 
 exportButton = navegadorFake.find_element(By.XPATH, '/html/body/div[55]/div/div/div/div/div/div/div[3]/button').click()
 loadTime.sleep(10)
-
-#checkoutImage = action.locateOnScreen("C:\\Users\\ctt\\Pictures\\imageCheck.png",1.5,confidence=0.7)
-#checkboxButton = navegadorFake.find_elements(By.CLASS_NAME,'input_9fc46df967')
-#for checkbox in checkboxButton:
-#    if checkoutImage is not None:
-#        checkbox.click()
-
